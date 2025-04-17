f951cbcd777c7801ec295d960c0dcf108eae4b3e
Amy Z <xmzhao000@gmail.com>
2025-04-18 05:31:40 +1000

    implemented the class RequestMessage and add in constuctors (for the sake of sending JSON request to the server)

---
+++ src/RequestMessage.java
@@ -0,0 +1,37 @@
+import java.util.List;
+
+public class RequestMessage {
+    public String action;
+    public String word;
+    public String meaning;
+    public String newMeaning;
+    public String oldMeaning;
+    public List<String> meanings;
+    public RequestMessage(String action, String word) {
+        this.action = action; //querymeaning or removeword
+        this.word = word;
+    }
+
+    public RequestMessage(String action, String word, List<String> meanings) {
+        //for now, addword (and multiple new meanings if there is?)
+        this.action = action;
+        this.word = word;
+        this.meanings = meanings;//////
+    }
+
+    public RequestMessage(String action, String word, String meaning) {
+        // addmeaning
+        this.action = action;
+        this.word = word;
+        this.meaning = meaning;
+    }
+
+    public RequestMessage(String action, String word, String oldMeaning, String newMeaning) {
+        //updatemeaning
+        this.action = action;
+        this.word = word;
+        this.oldMeaning = oldMeaning;
+        this.newMeaning = newMeaning;
+    }
+
+}
