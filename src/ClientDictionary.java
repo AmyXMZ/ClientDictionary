96109f15d84849e0677b1516248bafc6c15d2bb0
Amy Z <xmzhao000@gmail.com>
2025-04-20 04:58:08 +1000

    added GUI, commented out the console version of the client side

--- src/ClientDictionary.java
+++ src/ClientDictionary.java
@@ -7,16 +7,19 @@ import java.net.UnknownHostException;
 import java.util.List;
 import java.util.Scanner;
 import com.google.gson.Gson;
+
+import javax.swing.*;
+
 public class ClientDictionary {
 
     // IP and port
     private static String ip = "localhost";
     private static int port = 3005;
-    private ClientGUI gui;
     private Socket socket;
     private DataInputStream input;
 
     private DataOutputStream output;
+    private JFrame frame;
 
     //constructor
     public ClientDictionary(String ip, int port) throws IOException {
@@ -59,107 +62,109 @@ public class ClientDictionary {
         }
     }
 
-    //client close connection (upon entering "quit")
-    public void clientCloseConnection(RequestMessage r){
-        RequestMessage quit = new RequestMessage("quit");
-        String jsonRequest = new Gson().toJson(quit);
-        try {
-            output.writeUTF(jsonRequest);
-        } catch (IOException e) {
-            System.out.println("I/O error: cannot send quit request to the server.");
-           // e.printStackTrace();
-        }
+    //client close connection (upon entering "quit" for console only)
+    public void clientCloseConnection(){
+//        RequestMessage quit = new RequestMessage("quit");
+//        String jsonRequest = new Gson().toJson(quit);
+//        try {
+//            output.writeUTF(jsonRequest);
+//        } catch (IOException e) {
+//            System.out.println("I/O error: cannot send quit request to the server.");
+//        }
 
-        finally {
-            try {
-                if (input != null) {
-                    input.close();
-                }
-                if (output != null) {
-                    output.close();
-                }
-                if (socket != null){
-                    socket.close();
-                }
-                System.out.println("Client socket closed successfully.");
-            } catch (IOException e) {
-                System.out.println("I/O Error: failed to close client socket.");
-                //e.printStackTrace();
+        try {
+            if (input != null) {
+                input.close();
             }
+            if (output != null) {
+                output.close();
+            }
+            if (socket != null){
+                socket.close();
+            }
+            //System.out.println("Client socket closed successfully.");
+        } catch (IOException e) {
+            //System.out.println("I/O Error: failed to close client socket.");
+            JOptionPane.showMessageDialog(frame, "I/O Error: failed to close client socket.");
         }
 
     }
     public static void main(String[] args)
     {
-
-        try(Scanner scanner = new Scanner(System.in);)
-        {
-            ClientDictionary clientInstance = new ClientDictionary(ip, port);
-            RequestMessage requestMessage = null;
-            boolean flag = true;
-            while(flag){
-                System.out.print("\nSelect a request (addword, removeword, querymeanings, addmeaning, updatemeaning, quit): ");
-                String action = scanner.nextLine().trim().toLowerCase();
-                switch(action){
-                    case "addword":
-                        System.out.print("Enter word to add: ");
-                        String word_to_add = scanner.nextLine().trim().toLowerCase();
-                        System.out.println("Enter the meaning for the word: ");
-                        String meaning = scanner.nextLine().trim().toLowerCase();
-                        requestMessage = new RequestMessage(action, word_to_add, meaning);
-                        break;
-                    case "removeword":
-                        System.out.print("Enter word to remove: ");
-                        String word_to_remove = scanner.nextLine().trim().toLowerCase();
-                        requestMessage = new RequestMessage(action, word_to_remove);
-                        break;
-                    case "querymeanings":
-                        System.out.print("Enter word to search: ");
-                        String word_query_meanings = scanner.nextLine().trim().toLowerCase();
-                        requestMessage = new RequestMessage(action, word_query_meanings);
-                        break;
-                    case "addmeaning":
-                        System.out.print("Enter the existing word: ");
-                        String existing_word = scanner.nextLine().trim().toLowerCase();
-                        System.out.println("Enter the additional meaning to add: ");
-                        String additional_meaning = scanner.nextLine().trim().toLowerCase();
-                        requestMessage = new RequestMessage(action, existing_word, additional_meaning);
-                        break;
-                    case "updatemeaning":
-                        System.out.print("Enter the word: ");
-                        String word_meaning_update = scanner.nextLine().trim().toLowerCase();
-                        System.out.println("Enter the existing meaning: ");
-                        String old_meaning = scanner.nextLine().trim().toLowerCase();
-                        System.out.println("Enter the new meaning: ");
-                        String new_meaning = scanner.nextLine().trim().toLowerCase();
-                        requestMessage = new RequestMessage(action, word_meaning_update, old_meaning, new_meaning);
-                        break;
-                    case "quit":
-                        clientInstance.clientCloseConnection(requestMessage);
-                        flag = false;
-                        continue;
-                    default:
-                        System.out.println("Invalid request. Please refer to the given action and try again.");
-                        continue;
-                }
-                if (requestMessage != null) {//send the json to server
-                    ResponseMessage response = clientInstance.sendToReceive(requestMessage);
-                    clientInstance.displayServerResponse(response);
-                }
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientGUI(ip, port);
             }
-
-        }
-        catch (UnknownHostException e)
-        {
-            System.out.println("Error: unknown host. Double check the host ip and port.");
-        } catch (ConnectException e) {
-            System.out.println("Error: unable to connect to server.");
-        } catch (SocketException e) {
-            System.out.println("Error: client lost connection during communication with server.");
-        } catch (IOException e)
-        {
-            System.out.println("I/O Error: client unable to communicate with server.");
-        }
+        });
+//        try(Scanner scanner = new Scanner(System.in);)
+//        {
+//            ClientDictionary clientInstance = new ClientDictionary(ip, port);
+//            RequestMessage requestMessage = null;
+//            boolean flag = true;
+//            while(flag){
+//                System.out.print("\nSelect a request (addword, removeword, querymeanings, addmeaning, updatemeaning, quit): ");
+//                String action = scanner.nextLine().trim().toLowerCase();
+//                switch(action){
+//                    case "addword":
+//                        System.out.print("Enter word to add: ");
+//                        String word_to_add = scanner.nextLine().trim().toLowerCase();
+//                        System.out.println("Enter the meaning for the word: ");
+//                        String meaning = scanner.nextLine().trim().toLowerCase();
+//                        requestMessage = new RequestMessage(action, word_to_add, meaning);
+//                        break;
+//                    case "removeword":
+//                        System.out.print("Enter word to remove: ");
+//                        String word_to_remove = scanner.nextLine().trim().toLowerCase();
+//                        requestMessage = new RequestMessage(action, word_to_remove);
+//                        break;
+//                    case "querymeanings":
+//                        System.out.print("Enter word to search: ");
+//                        String word_query_meanings = scanner.nextLine().trim().toLowerCase();
+//                        requestMessage = new RequestMessage(action, word_query_meanings);
+//                        break;
+//                    case "addmeaning":
+//                        System.out.print("Enter the existing word: ");
+//                        String existing_word = scanner.nextLine().trim().toLowerCase();
+//                        System.out.println("Enter the additional meaning to add: ");
+//                        String additional_meaning = scanner.nextLine().trim().toLowerCase();
+//                        requestMessage = new RequestMessage(action, existing_word, additional_meaning);
+//                        break;
+//                    case "updatemeaning":
+//                        System.out.print("Enter the word: ");
+//                        String word_meaning_update = scanner.nextLine().trim().toLowerCase();
+//                        System.out.println("Enter the existing meaning: ");
+//                        String old_meaning = scanner.nextLine().trim().toLowerCase();
+//                        System.out.println("Enter the new meaning: ");
+//                        String new_meaning = scanner.nextLine().trim().toLowerCase();
+//                        requestMessage = new RequestMessage(action, word_meaning_update, old_meaning, new_meaning);
+//                        break;
+//                    case "quit":
+//                        clientInstance.clientCloseConnection(requestMessage);
+//                        flag = false;
+//                        continue;
+//                    default:
+//                        System.out.println("Invalid request. Please refer to the given action and try again.");
+//                        continue;
+//                }
+//                if (requestMessage != null) {//send the json to server
+//                    ResponseMessage response = clientInstance.sendToReceive(requestMessage);
+//                    clientInstance.displayServerResponse(response);
+//                }
+//            }
+//
+//        }
+//        catch (UnknownHostException e)
+//        {
+//            System.out.println("Error: unknown host. Double check the host ip and port.");
+//        } catch (ConnectException e) {
+//            System.out.println("Error: unable to connect to server.");
+//        } catch (SocketException e) {
+//            System.out.println("Error: client lost connection during communication with server.");
+//        } catch (IOException e)
+//        {
+//            System.out.println("I/O Error: client unable to communicate with server.");
+//        }
     }
 
 }
--- src/ClientGUI.java
+++ src/ClientGUI.java
@@ -1,23 +1,181 @@
+
 import javax.swing.*;
-import java.awt.event.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.List;
+
 public class ClientGUI {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("My Dictionary");
+    private ClientDictionary client;
 
-        JButton button = new JButton("Search");
-        button.setBounds(130, 100, 100, 40);
+    private JFrame frame;
+    private JComboBox<String> select;
+    private JTextField word;
+    private JTextField meaning;
+    private JTextField oldmeaning;
+    private JTextField newmeaning;
+    private JButton submit;
+    private JTextArea responseArea;
 
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(frame, "Button clicked!");
+    public ClientGUI(String ip, int port) {
+        try {
+            client = new ClientDictionary(ip, port);
+        } catch (UnknownHostException e){
+            JOptionPane.showMessageDialog(frame, "Error: unknown host. Double check the host ip and port.");
+            return;
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(frame, "I/O Error: client unable to communicate with server.");
+            return;
+        }
+        initializeFrame();
+    }
+    // initialize the window (frame)
+    private void initializeFrame() {
+        frame = new JFrame("Dictionary Client");
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                client.clientCloseConnection();
+                frame.dispose();
+                System.exit(0);
             }
         });
+        frame.setSize(550, 350);
+        frame.setLayout(new BorderLayout());
 
-        frame.add(button);
-        frame.setSize(400, 300);
-        frame.setLayout(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        prepareComponents();
         frame.setVisible(true);
     }
+
+    //initialize components within the frame
+    private void prepareComponents(){
+        JPanel p = new JPanel(new GridLayout(6, 2));
+
+        select = new JComboBox<>(new String[]{
+                "querymeanings", "addword", "removeword", "addmeaning", "updatemeaning"
+        });
+        word = new JTextField();
+        meaning = new JTextField();
+        oldmeaning = new JTextField();
+        newmeaning = new JTextField();
+
+        p.add(new JLabel("Select Request:"));
+        p.add(select);
+        p.add(new JLabel("Word:"));
+        p.add(word);
+        p.add(new JLabel("Meaning:"));
+        p.add(meaning);
+        p.add(new JLabel("Old Meaning (for update):"));
+        p.add(oldmeaning);
+        p.add(new JLabel("New Meaning:"));
+        p.add(newmeaning);
+
+        submit = new JButton("Submit");
+        JPanel sp = new JPanel(new BorderLayout());
+        sp.add(submit, BorderLayout.CENTER);
+        p.add(submit, BorderLayout.CENTER);
+        frame.add(sp, BorderLayout.SOUTH);
+        frame.add(p, BorderLayout.NORTH);
+
+        responseArea = new JTextArea();
+        responseArea.setEditable(false);
+        frame.add(new JScrollPane(responseArea), BorderLayout.CENTER);
+
+        submit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleRequest();
+            }
+        });
+        select.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showHideInput();
+            }
+        });
+// show/hide fields based on the request selected
+        showHideInput();
+    }
+
+    private void showHideInput() {
+        String request;
+        request = (String) select.getSelectedItem();
+        meaning.setEnabled(false);
+        oldmeaning.setEnabled(false);
+        newmeaning.setEnabled(false);
+
+        if (request.equals("addword")) {
+            meaning.setEnabled(true);
+        }if(request.equals("addmeaning")){
+            meaning.setEnabled(true);
+        }else if (request.equals("updatemeaning")) {
+            oldmeaning.setEnabled(true);
+            newmeaning.setEnabled(true);
+        }
+    }
+
+    private void handleRequest() {
+        String action = (String) select.getSelectedItem();
+        String word1 = word.getText().trim().toLowerCase();
+        String meaning1 = meaning.getText().trim().toLowerCase();
+        String oldMeaning1 = oldmeaning.getText().trim().toLowerCase();
+        String newMeaning1 = newmeaning.getText().trim().toLowerCase();
+        try {
+            RequestMessage request;
+
+            switch (action) {
+                case "addword":
+                case "addmeaning":
+                    request = new RequestMessage(action, word1, meaning1);
+                    break;
+                case "updatemeaning":
+                    request = new RequestMessage(action, word1, oldMeaning1, newMeaning1);
+                    break;
+                case "querymeanings":
+                case "removeword":
+                default:
+                    request = new RequestMessage(action, word1);
+                    break;
+            }
+
+            ResponseMessage response = client.sendToReceive(request);
+            displayServerResponse(response);
+        } catch (ConnectException e) {
+            responseArea.setText("Error: unable to connect to server.");
+        } catch (SocketException e) {
+            responseArea.setText("Error: client lost connection during communication with server.");
+        } catch (IOException e) {
+            responseArea.setText("Error: client unable to communicate with server: " + e.getMessage());
+        }
+    }
+
+    private void displayServerResponse(ResponseMessage response) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Status").append(": ").append(response.getStatus()).append("\n");
+
+        if (response.getStatus().equalsIgnoreCase("Success")) {
+            List<String> meanings = response.getMeanings();
+            if (!meanings.isEmpty()) {
+                sb.append("Meanings:\n");
+                for (String m : meanings) {
+                    sb.append(" -> ").append(m).append("\n");
+                }
+            } else {
+                sb.append("This request has been completed successfully!\n");
+            }
+        } else {
+            sb.append(response.getStatus()).append(": ").append(response.getErrorMessage()).append("\n");
+        }
+
+        responseArea.setText(sb.toString());
+    }
+
 }
 
